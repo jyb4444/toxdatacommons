f10bb67d8504921151aba5ace7fdd6df09c9d0f8
keji yuan <kejiyuan@kejideMacBook-Pro.local>
2024-03-01 12:49:38 -0500

    test

--- custom_configs/schema_compile.py
+++ custom_configs/schema_compile.py
@@ -11,9 +11,9 @@ from dictionaryutils import dump_schemas_from_dir
 import json
 
 #import dictionaryutils
-schema = dump_schemas_from_dir(directory="./gdcdictionary/schemas/tox_model")
+schema = dump_schemas_from_dir(directory="../gdcdictionary/schemas/tox_model")
 schema["_settings.yaml"]["_dict_version"] = "0.1"
 
-with open('./custom_configs/schema.json', 'w') as json_file:
+with open('../custom_configs/schema.json', 'w') as json_file:
     json.dump(schema, json_file)
 
